78646489073c0f81f41fe2a075b63b75b5e2d2ae
Leon Teichroeb <bigleon00@gmail.com>
2022-04-30 03:38:46 +0200

    Fixed warm reboot

--- source/monoterm/restart.c
+++ source/monoterm/restart.c
@@ -1,13 +1,13 @@
 #include <kernel/print.h>
 #include "bindings.h"
 
-extern void _start();
+extern void el1_entry();
 
 
 int monoterm_restart(int argc, char *argv[]) {
     uart_print("Restarting...\r\n");
     // TODO: Wrap this up in a neat kernel function
-    _start();
+    el1_entry();
     // This return should never be called...
     return 0;
 }
